39215fa490ef53764150dae892c94fca52e30714
Ryan Zulkoski <ryan.zulkoski@gmail.com>
2013-02-20 11:24:01 -0600

    Added Flickr helper files to project and entered my own API key into FlickerAPIKey.h

---
+++ SPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,12 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"9a438d1d1c76b8f48d2642218a2d8bdb"
